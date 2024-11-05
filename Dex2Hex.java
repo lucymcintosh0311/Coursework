c85aa0c3070f4f89b52a6a2ed9e4de9fe52829ac
“lucymcintosh0311” <lucymcintosh0311@icloud.com>
2024-11-05 11:29:33 +0000

    Automatic Build Test for jenkins

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -4,6 +4,7 @@ class Dex2Hex
 {
 public static int Arg1;
     public static void main(String args[])    {
+	print("Automatic Build Test for Jenkins")
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
