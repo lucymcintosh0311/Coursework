5c712cc702205f14d8b05da503cf3be82a1c2f5d
“lucymcintosh0311” <lucymcintosh0311@icloud.com>
2024-11-09 23:02:44 +0000

    Unimporved code check

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,33 +1,16 @@
 import java.util.Scanner;
 
-public class Dex2Hex{
-    public static int Arg1;
-    public static void main(String args[]) {
-	// Check if the user provided an input
-        if (args.length < 1) {
-            System.out.println("Input Error: Please provide a number input.");
-            return;
-	}	
-        int num2;
-
-        // Try to parse the provided argument into an integer
-        try {
-            num2 = Integer.parseInt(args[0]);
-        } 
-	catch (NumberFormatException e) {
-
-            // If the argument is not a valid integer, print an error message
-            System.out.println("Input Error: Invalid input. Please enter a valid integer.");
-            return;
-        }
-
+class Dex2Hex
+{
+public static int Arg1;
+    public static void main(String args[])    {
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-	System.out.println("Automatic Build Test for Jenkins");
+
         while(num != 0)
         {
             rem=num%16;
@@ -38,5 +21,5 @@ public class Dex2Hex{
         System.out.println("Hexadecimal representation is: " + hexadecimal);
 
     }
-
 }
+
