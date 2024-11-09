21004cac1e0440cb8e144f952793bb1a37bf11dc
“lucymcintosh0311” <lucymcintosh0311@icloud.com>
2024-11-09 23:11:03 +0000

    another commit

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
 
-class Dex2Hex
-{
-public static int Arg1;
-    public static void main(String args[])    {
+public class Dex2Hex{
+    public static int Arg1;
+    public static void main(String args[]) {
+        // Check if the user provided an input
+        if (args.length < 1) {
+            System.out.println("Input Error: Please provide a number input.");
+            return;
+        }
+        int num2;
+
+        // Try to parse the provided argument into an integer
+        try {
+            num2 = Integer.parseInt(args[0]);
+        }
+        catch (NumberFormatException e) {
+
+            // If the argument is not a valid integer, print an error message
+            System.out.println("Input Error: Invalid input. Please enter a valid integer.");
+            return;
+        }
+
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
@@ -21,5 +38,6 @@ public static int Arg1;
         System.out.println("Hexadecimal representation is: " + hexadecimal);
 
     }
+
 }
 
