c275e5600c6d230c960c42c26fcb277c76bcfb25
“lucymcintosh0311” <lucymcintosh0311@icloud.com>
2024-11-09 23:53:55 +0000

    Back to origional code to see failed tests

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,24 +1,8 @@
 import java.util.Scanner;
 
-public class Dex2Hex {
-public final int Arg1;
+class Dex2Hex {
+public static int Arg1;
     public static void main(String args[]) {
-        // Check if the user provided an input
-        if (args.length < 1) {
-            System.out.println("Input Error: Please provide a number input.");
-            return;
-        }
-
-        // Try to parse the provided argument into an integer
-        try {
-            int num2 = Integer.parseInt(args[0]);
-        }
-        catch (NumberFormatException e) {
-
-        // If the argument is not a valid integer, print an error message
-            System.out.println("Input Error: Invalid input. Please enter a valid integer.");
-            return;
-        }
 
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
