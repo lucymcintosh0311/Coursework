e9bc6c08d0656b4ce640725ff140575cd29bbfff
“lucymcintosh0311” <lucymcintosh0311@icloud.com>
2024-11-10 01:00:46 +0000

    move array [] to the type

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -4,7 +4,7 @@ public class Dex2Hex {
 public static int Arg1;
 private static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // Check if the user provided an input
         if (args.length < 1) {
             logger.severe("Input Error: Please provide a number input.");
@@ -23,7 +23,7 @@ private static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
         }
 
         Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] ch={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem;
 	int num;
         num = Arg1;
