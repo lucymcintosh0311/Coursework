e4fda37106ea7020517284ec33b585a7942ec7dc
“lucymcintosh0311” <lucymcintosh0311@icloud.com>
2024-11-10 01:10:33 +0000

    change Arg1 ro arg1 to follow the regular expressions guide

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,7 +1,7 @@
 import java.util.logging.Logger;
 
 public class Dex2Hex {
-public static int Arg1;
+public static int arg1;
 private static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
 
     public static void main(String[] args) {
@@ -13,7 +13,7 @@ private static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
 
         // Try to parse the provided argument into an integer
         try {
-            Arg1 = Integer.parseInt(args[0]);
+            arg1 = Integer.parseInt(args[0]);
         }
         catch (NumberFormatException e) {
 
@@ -22,11 +22,11 @@ private static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
             return;
         }
 
-        Arg1 = Integer.parseInt(args[0]);
+        arg1 = Integer.parseInt(args[0]);
         char[] ch={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem;
 	int num;
-        num = Arg1;
+        num = arg1;
         String hexadecimal="";
         logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
 
